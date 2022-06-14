8c0d3e61de6f1fb7929d0ea1afac58a7c26fcd31
TheBlackSpear <70425627+TheBlackSpear@users.noreply.github.com>
2022-06-14 00:22:37 +0000

    Update config.php

--- superbrain.net/config/config.php
+++ superbrain.net/config/config.php
@@ -6,9 +6,9 @@
 <?php 
 
 return array(
-	"db_host"=>"infocimbeventdd.mysql.db",
+	"db_host"=>"",
 	"db_port"=>"3306",
-	"db_name"=>"infocimbeventdd",
-	"db_user"=>"infocimbeventdd",
-	"db_pass"=>"ci2020S3rVeMe"
+	"db_name"=>"",
+	"db_user"=>"",
+	"db_pass"=>""
 	); 
